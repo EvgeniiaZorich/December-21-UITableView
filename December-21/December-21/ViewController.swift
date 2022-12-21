b2ecf959319d735c6c9704c800c0090716da0a54
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2022-12-21 21:06:37 +0100

    initial commit

---
+++ December-21/December-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  December-21
+//
+//  Created by Евгения Зорич on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
